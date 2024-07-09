73d2c5da180693ccf523141d5e0a71d4021df351
Victor dS <victordespirlet@gmail.com>
2024-07-09 17:10:14 +0200

    Add files via upload

---
+++ Menu/main.js
@@ -0,0 +1,13 @@
+const toggleMenu = () => {
+    document.body.classList.toggle("open")
+}
+
+const swiper = new Swiper(".swiper", {
+    loop: true,
+    speed: 750,
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    })
