73dbad73341dd538e1586c56f980f1a44b14ba87
Victor dS <victordespirlet@gmail.com>
2024-09-20 19:11:21 +0200

    Add files via upload

---
+++ Simple Search Bar/app.js
@@ -0,0 +1,36 @@
+var list = [
+  "Banana",
+  "Apple",
+  "Orange",
+  "Blueberry",
+  "Abricot",
+  "Kiwi",
+  "Pear",
+];
+var myList = document.getElementById("myList");
+
+function filterList(searchTerm) {
+  myList.innerHTML = "";
+
+  var filteredList = list.filter(function (item) {
+    return item.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
+  filteredList.forEach(function (item) {
+    var li = document.createElement("li");
+    li.textContent = item;
+    myList.appendChild(li);
+  });
+}
+
+var searchInput = document.getElementById("searchInput");
+searchInput.addEventListener("keyup", function (event) {
+  var searchTerm = event.target.value;
+  filterList(searchTerm);
+});
+
+list.forEach(function (item) {
+  var li = document.createElement("li");
+  li.textContent = item;
+  myList.appendChild(li);
+});
