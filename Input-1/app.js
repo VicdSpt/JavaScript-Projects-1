2e05433fe829b3277e9f8ee523b3c0a2df3b9a72
Victor dS <victordespirlet@gmail.com>
2024-09-29 21:11:20 +0200

    Add files via upload

---
+++ Input-1/app.js
@@ -0,0 +1,22 @@
+const input = document.querySelector(".input-field");
+const inputIcon = document.querySelector(".input-icon");
+
+inputIcon.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    inputIcon.setAttribute(
+        'src', 
+        input.getAttribute('type') === 'password' ?
+        'images/eye-off.svg'
+          :
+        'images/eye.svg'
+    );
+
+    input.setAttribute(
+        'type', 
+        input.getAttribute('type') === 'password' ? 
+        'text'
+          :
+        'password'
+    );
+});
